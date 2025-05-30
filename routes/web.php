8c9a6b3b59b18092f7d0e931a6355c02f1b39561
joshuatheop <joshuatheo1604@gmail.com>
2025-05-30 17:07:24 +0700

    login register fixed

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\AttachmentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
 
 Route::get('/dashboard', function () {
